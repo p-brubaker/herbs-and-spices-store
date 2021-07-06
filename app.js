2d32308133d0a46aa6ca27750ed9d1f6416833d3
Paul Brubaker <pbrubak@gmail.com>
2021-07-06 15:32:28 -0700

    add products array

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 // herbs and spices data model
-  // id: a unique string that identifies the product
-  // name: user friendly name of the product
-  // image: image file path
-  // description: a longer discription of the product
-  // category: the category this product belongs to
-  // price: the price the user will pay
+// id: a unique string that identifies the product
+// name: user friendly name of the product
+// image: image file path
+// description: a longer discription of the product
+// category: the category this product belongs to
+// price: the price the user will pay
+
+import { products } from './products.js';
+
---
+++ products.js
@@ -0,0 +1,42 @@
+export const products = [
+    {
+        name: 'saffron',
+        id: 'crocus-sativas',
+        category: 'spice',
+        description: 'The most expensive spice',
+        image: './assets/saffron',
+        price: 2336.00
+    },
+    {
+        name: 'garlic',
+        id: 'allium-sativum',
+        category: 'herb',
+        description: 'An herb with a pungent kick',
+        image: './assets/garlic',
+        price: 0.50
+    },
+    {
+        name: 'basil',
+        id: 'ocimum-basilicum',
+        category: 'herb',
+        description: 'Smells great, best when fresh',
+        image: './assets/basil',
+        price: 5.00
+    },
+    {
+        name: 'cayenne',
+        id: 'c-annum',
+        category: 'spice',
+        description: 'A moderately hot pepper',
+        image: './assets/cayenne',
+        price: 5.00
+    },
+    {
+        name: 'cloves',
+        id: 'syzygium-aromaticum',
+        category: 'spice',
+        description: 'Good for hot beverages',
+        image: './assets/cloves',
+        price: 10.00
+    }
+];
