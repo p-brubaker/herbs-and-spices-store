acd8ecea10d59537257f656d6dbfa2e8448d27a9
Paul Brubaker <pbrubak@gmail.com>
2021-07-06 15:53:28 -0700

    add DOM render function

--- app.js
+++ app.js
@@ -8,3 +8,30 @@
 
 import { products } from './products.js';
 
+function renderProduct(product) {
+    const li = document.createElement('li');
+    const productDiv = document.createElement('div.store-item');
+    const buyItemDiv = document.createElement('div.buy-item');
+    const addButton = document.createElement('button.add-to-basket');
+
+    const nameSpan = document.createElement('span.name');
+    nameSpan.innerText = product.name;
+
+    const img = document.createElement('img');
+    img.src = product.image;
+
+    const descSpan = document.createElement('span.description');
+    descSpan.innerText = product.description;
+
+    
+    const priceSpan = document.createElement('span.price');
+    priceSpan.innerText = product.price;
+
+    buyItemDiv.appendChild(priceSpan);
+    buyItemDiv.appendChild(addButton);
+    productDiv.appendChild(nameSpan);
+    productDiv.appendChild(img);
+    productDiv.appendChild(descSpan);
+    productDiv.appendChild(buyItemDiv);
+    li.appendChild(productDiv);
+}
