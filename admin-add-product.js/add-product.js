9860530afc2f95f8482a77b0005db81341fea560
Paul Brubaker <pbrubak@gmail.com>
2021-07-16 16:42:20 -0700

    add admin page

---
+++ admin-add-product.js/add-product.js
@@ -0,0 +1,37 @@
+import { products } from '../data/products.js';
+
+import { getProducts, setProducts } from '../utils.js';
+
+const nameInput = document.getElementById('product-name');
+const categoryInput = document.getElementById('category');
+const productDescription = document.getElementById('product-description');
+const productImageInput = document.getElementById('product-image');
+const productPriceInput = document.getElementById('product-price');
+const productSubmit = document.getElementById('product-submit');
+
+function getNewId(currentProducts) {
+    
+    return currentProducts.reduce((a, b) => {
+        if (a.id > b.id) {
+            return a;
+        } else {
+            return b;
+        }
+    }).id + 1;
+} 
+
+productSubmit.addEventListener('click', () => {
+
+    let inventory = getProducts() || products;
+
+    let newProduct = {};
+    newProduct.name = nameInput.value;
+    newProduct.category = categoryInput.value;
+    newProduct.description = productDescription.value;
+    newProduct.image = productImageInput.value;
+    newProduct.id = getNewId(products);
+    newProduct.price = Number(productPriceInput.value);
+    inventory = [...inventory, newProduct];
+
+    setProducts(inventory);
+});
--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 import { products } from './data/products.js';
 import { renderProduct } from './renderProducts.js';
+import { getProducts, setProducts } from './utils.js';
 
 const productList = document.getElementById('product-list');
 
-for (let product in products) {
-    productList.appendChild(renderProduct(products[product]));
+
+const inventory = getProducts() || products;
+
+for (let product in inventory) {
+    productList.appendChild(renderProduct(inventory[product]));
 }
